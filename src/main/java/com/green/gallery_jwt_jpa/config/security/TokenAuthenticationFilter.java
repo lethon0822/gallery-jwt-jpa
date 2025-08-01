33576e61568eed9e3f8e87a2255ea484b0204930
sbsteacher <id@gmail.com>
2025-08-01 16:11:00 +0900

    JWT - TokenAuthenticationFilter 완료

--- src/main/java/com/green/gallery_jwt_jpa/config/security/TokenAuthenticationFilter.java
+++ src/main/java/com/green/gallery_jwt_jpa/config/security/TokenAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package com.green.gallery_jwt_jpa.config.security;
 
+import com.green.gallery_jwt_jpa.config.jwt.JwtTokenManager;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -13,11 +16,15 @@ import java.io.IOException;
 @Slf4j
 @RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtTokenManager jwtTokenManager;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //토큰 처리
-
-
+        Authentication authentication = jwtTokenManager.getAuthentication(request);
+        if (authentication != null) {
+            SecurityContextHolder.getContext().setAuthentication(authentication); //인증 처리
+        }
         filterChain.doFilter(request, response); //다음 필터에게 req, res 넘기기
     }
 }
