cc4d21af27bd649712a12fff1c9b3b8a6c66d8d8
1ethon0822 <1ethyjj9240@gmail.com>
2025-08-01 17:41:23 +0900

    수정

--- src/main/java/com/green/gallery_jwt_jpa/config/security/WebSecurityConfiguration.java
+++ src/main/java/com/green/gallery_jwt_jpa/config/security/WebSecurityConfiguration.java
@@ -18,10 +18,11 @@ Bean 메소드는 무조건 싱글톤으로 처리된다.
 public class WebSecurityConfiguration {
 
     private final TokenAuthenticationFilter tokenAuthenticationFilter;
+    private final TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint;
 
-    //Bean 메소드
+    //Bean
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, TokenAuthenticationEntryPoint tokenAuthenticationEntryPoint) throws Exception {
         //람다식
         return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //security가 session을 사용하지 않는다.
                    .httpBasic(httpBasicSpec -> httpBasicSpec.disable()) //시큐리티가 제공해주는 인증 처리 -> 사용 안 함
@@ -29,10 +30,11 @@ public class WebSecurityConfiguration {
                    .csrf(csrfSpec -> csrfSpec.disable()) // BE - csrf라는 공격이 있는데 공격을 막는 것이 기본으로 활성화 되어 있는데
                                                         // 세션을 이용한 공격이다. 세션을 어차피 안 쓰니까 비활성화
                    .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/cart").authenticated()
-                                                    .requestMatchers(HttpMethod.POST, "/api/v1/item").authenticated()
+                                                    .requestMatchers(HttpMethod.POST, "/api/v1/item").hasRole("USER_2")
                                                     .anyRequest().permitAll()
                    )
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(e -> e.authenticationEntryPoint(tokenAuthenticationEntryPoint))
                    .build();
     }
 
