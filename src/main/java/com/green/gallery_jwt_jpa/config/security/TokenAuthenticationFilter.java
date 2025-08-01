5f0bd89242c9ae1cf4d759c3631a6a887e52e01e
sbsteacher <id@gmail.com>
2025-08-01 15:57:04 +0900

    JWT - UserPrincipal

---
+++ src/main/java/com/green/gallery_jwt_jpa/config/security/TokenAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.green.gallery_jwt_jpa.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        //토큰 처리
+
+
+        filterChain.doFilter(request, response); //다음 필터에게 req, res 넘기기
+    }
+}
