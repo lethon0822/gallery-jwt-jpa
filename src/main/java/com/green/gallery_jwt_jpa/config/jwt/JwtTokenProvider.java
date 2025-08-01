eb276f4260899f62948fd9c3292f35b3d97c8c75
sbsteacher <id@gmail.com>
2025-08-01 13:12:15 +0900

    JWT - 0801 오전 작업

--- src/main/java/com/green/gallery_jwt_jpa/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/green/gallery_jwt_jpa/config/jwt/JwtTokenProvider.java
@@ -8,7 +8,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
