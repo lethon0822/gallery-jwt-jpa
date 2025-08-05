d0b79429b1a99fb057c72244e47ccb7c6a479d91
sbsteacher <id@gmail.com>
2025-08-05 11:11:43 +0900

    회원가입 성공

---
+++ src/main/java/com/green/gallery_jwt_jpa/entity/MembersRoles.java
@@ -0,0 +1,24 @@
+package com.green.gallery_jwt_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+public class MembersRoles {
+
+    @EmbeddedId
+    private MembersRolesIds membersRolesIds;
+
+    //관계 설정
+    //CascadeType.PERSIST 영속성 전이
+    //CascadeType.REMOVE 부모 영속성이 delete되면 본인도 delete
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    @MapsId("memberId")
+    private Members members;
+
+}
